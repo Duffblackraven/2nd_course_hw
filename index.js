5d2ad77b18059e68fd19830312c831692985f288
duffblackraven <t9164700668@gmail.com>
2023-09-10 11:18:08 +0200

    Changed file index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ alert (a);
 
 // Task 2
 let yearRelease=Number (2007);
-alert (`year release first iPhone: ${yearRelease}`);
+alert (`Year release first iPhone: ${yearRelease}`);
 
 // Task 3
 let nameCreator=String (`Брендан Эйх`);
@@ -16,13 +16,13 @@ alert (`Имя создателя языка JavaScript: ${nameCreator}`)
 let b=Number(10);
 let c=Number(2);
 let sum=(b+c);
-alert (`Сумма= ${sum}`)
+alert (`Сумма 10 и 2= ${sum}`)
 let differens=(b-c);
-alert (`Разность= ${differens}`);
+alert (`Разность 10 и 2= ${differens}`);
 let multiplication=(b*c);
-alert (`Произведение= ${multiplication}`);
+alert (`Произведение 10 и 2= ${multiplication}`);
 let division=(b/c);
-alert (`Частное= ${division}`);
+alert (`Частное 10 и 2= ${division}`);
 
 // Task 5
 let result=(2**5);
@@ -32,7 +32,7 @@ alert (`Возведение 2 в 5-ю степень= ${result}`);
 let d=Number(9);
 let e=Number(2);
 let divide= (d%e);
-alert (`Остаток от деления= ${divide}`)
+alert (`Остаток от деления 9 и 2= ${divide}`)
 
 // Task 7
 let num = 1;
@@ -47,4 +47,29 @@ alert(num);
 //Task 8
 let age=prompt (`Сколько вам лет?`)
 alert (age)
+
+// Task 9.0
+const user= {name: `Pelmeshki`,age:10,isAdmin:true};
+alert (user.name);
+alert (user.age);
+alert (user.isAdmin);
+
+//Task 9.1
+user [`city of residence`];
+alert ([`city of residence`]);
+
+// Task 9.2
+user.age=15;
+alert(user.age);
+
+// Task 9.3
+delete user [`City of residence`];
+
+// Task 9.4
+let info=prompt (`Какую информацию хотите узнать о пользователе?`)
+alert (user[info])
+
+// Task 10
+let userName=prompt (`Как ва зовут?`);
+alert (`Привет, ${userName} !`)
 
