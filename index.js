299a1f0a333700c8c937efccbd31ddbb289a14a7
duffblackraven <t9164700668@gmail.com>
2023-09-10 09:31:54 +0200

    Changed file index.js

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+// Task 1
+let a=Number(10);
+alert (a);
+a=Number(20);
+alert (a);
 
+// Task 2
+let yearRelease=Number (2007);
+alert (`year release first iPhone: ${yearRelease}`);
 
+// Task 3
+let nameCreator=String (`Брендан Эйх`);
+alert (`Имя создателя языка JavaScript: ${nameCreator}`)
 
