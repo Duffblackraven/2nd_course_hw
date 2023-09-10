b11f7159e6c03c477bf8429d6b1de2d2f8a13a75
duffblackraven <t9164700668@gmail.com>
2023-09-10 10:27:20 +0200

    Changed file index.js

--- index.js
+++ index.js
@@ -12,3 +12,39 @@ alert (`year release first iPhone: ${yearRelease}`);
 let nameCreator=String (`Брендан Эйх`);
 alert (`Имя создателя языка JavaScript: ${nameCreator}`)
 
+// Task 4
+let b=Number(10);
+let c=Number(2);
+let sum=(b+c);
+alert (`Сумма= ${sum}`)
+let differens=(b-c);
+alert (`Разность= ${differens}`);
+let multiplication=(b*c);
+alert (`Произведение= ${multiplication}`);
+let division=(b/c);
+alert (`Частное= ${division}`);
+
+// Task 5
+let result=(2**5);
+alert (`Возведение 2 в 5-ю степень= ${result}`);
+
+// Task 6
+let d=Number(9);
+let e=Number(2);
+let divide= (d%e);
+alert (`Остаток от деления= ${divide}`)
+
+// Task 7
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num = ++num;
+num = --num;
+alert(num);
+
+//Task 8
+let age=prompt (`Сколько вам лет?`)
+alert (age)
+
