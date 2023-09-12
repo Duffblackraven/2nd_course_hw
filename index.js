5e05f93fa179eeb15810fdb30677299c6478232e
duffblackraven <t9164700668@gmail.com>
2023-09-12 09:24:54 +0200

    Changed file index.js

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
 // Task 1
-let a=Number(10);
+let a=10;
 alert (a);
-a=Number(20);
+a=20;
 alert (a);
 
 // Task 2
-let yearRelease=Number (2007);
+let yearRelease=2007;
 alert (`Year release first iPhone: ${yearRelease}`);
 
 // Task 3
 let nameCreator=String (`Брендан Эйх`);
-alert (`Имя создателя языка JavaScript: ${nameCreator}`)
+alert (`Имя создателя языка JavaScript: ${nameCreator}`);
 
 // Task 4
-let b=Number(10);
-let c=Number(2);
-let sum=(b+c);
+let b=10;
+let c=2;
+let sum=b+c;
 alert (`Сумма 10 и 2= ${sum}`)
-let differens=(b-c);
+let differens=b-c;
 alert (`Разность 10 и 2= ${differens}`);
-let multiplication=(b*c);
+let multiplication=b*c;
 alert (`Произведение 10 и 2= ${multiplication}`);
-let division=(b/c);
+let division=b/c;
 alert (`Частное 10 и 2= ${division}`);
 
 // Task 5
-let result=(2**5);
+let result=2**5;
 alert (`Возведение 2 в 5-ю степень= ${result}`);
 
 // Task 6
-let d=Number(9);
-let e=Number(2);
-let divide= (d%e);
+let d=9;
+let e=2;
+let divide= d%e;
 alert (`Остаток от деления 9 и 2= ${divide}`)
 
 // Task 7
@@ -55,8 +55,8 @@ alert (user.age);
 alert (user.isAdmin);
 
 //Task 9.1
-user [`city of residence`];
-alert ([`city of residence`]);
+user [`City of residence`];
+alert ([`City of residence`]);
 
 // Task 9.2
 user.age=15;
