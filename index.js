2dd3cd675bfc9135809387360be9332c913869d0
duffblackraven <t9164700668@gmail.com>
2023-09-17 08:21:40 +0200

    Changed file index.js

--- index.js
+++ index.js
@@ -101,43 +101,27 @@ b='3';
 alert (Number(a) + Number(b))
 
 // Task 5
-let monthNumber=String(prompt(`Номер месяца`));
+let monthNumber=Number(prompt(`Введите номер месяца`));
 switch (monthNumber) {
-    case `1`:
-        alert(`Зима`);    
+    case 1:
+    case 2:
+    case 12:
+    alert(`Зима`);    
     break;
-    case `2`:
-        alert(`Зима`);
+    case 3:
+    case 4:
+    case 5:
+    alert(`Весна`);
     break;
-    case `3`:
-        alert(`Весна`);
+    case 6:
+    case 7:
+    case 8:
+    alert(`Лето`);
     break;
-    case `4`:
-        alert(`Весна`);
-    break;
-    case `5`:
-        alert(`Весна`);
-    break;
-    case `6`:
-        alert(`Лето`);
-    break;
-    case `7`:
-        alert(`Лето`);
-    break;
-    case `8`:
-        alert(`Лето`);
-    break;
-    case `9`:
-        alert(`Осень`);
-    break;
-    case `10`:
-        alert(`Осень`);
-    break;
-    case `11`:
-        alert(`Осень`);
-    break;
-    case `12`:
-        alert(`Зима`);
+    case 9:
+    case 10:
+    case 11:
+    alert(`Осень`);
     break;
     default: (alert(`Попробуй снова`))
         break;
