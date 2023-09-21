c42da92faed4a0792715114322328b9bdd695858
duffblackraven <t9164700668@gmail.com>
2023-09-21 09:45:20 +0200

    Changed file index.js

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 // Task 1
-let a=10;
-alert (a);
-a=20;
-alert (a);
+let a = 10;
+console.log (a);
+a = 20;
+console.log (a);
 
 // Task 2
-let yearRelease=2007;
-alert (`Year release first iPhone: ${yearRelease}`);
+let yearRelease = 2007;
+console.log (`Year release first iPhone: ${yearRelease}`);
 
 // Task 3
-let nameCreator=String (`Брендан Эйх`);
-alert (`Имя создателя языка JavaScript: ${nameCreator}`);
+let nameCreator = String (`Брендан Эйх`);
+console.log (`Имя создателя языка JavaScript: ${nameCreator}`);
 
 // Task 4
-let b=10;
-let c=2;
-let sum=b+c;
-alert (`Сумма 10 и 2= ${sum}`)
-let differens=b-c;
-alert (`Разность 10 и 2= ${differens}`);
-let multiplication=b*c;
-alert (`Произведение 10 и 2= ${multiplication}`);
-let division=b/c;
-alert (`Частное 10 и 2= ${division}`);
+let b = 10;
+let c = 2;
+let sum = b+c;
+console.log (`Сумма 10 и 2= ${sum}`)
+let differens = b-c;
+console.log (`Разность 10 и 2= ${differens}`);
+let multiplication = b*c;
+console.log (`Произведение 10 и 2= ${multiplication}`);
+let division = b/c;
+console.log (`Частное 10 и 2= ${division}`);
 
 // Task 5
-let result=2**5;
-alert (`Возведение 2 в 5-ю степень= ${result}`);
+let result = 2**5;
+console.log (`Возведение 2 в 5-ю степень= ${result}`);
 
 // Task 6
-let d=9;
-let e=2;
-let divide= d%e;
-alert (`Остаток от деления 9 и 2= ${divide}`)
+let d = 9;
+let e = 2;
+let divide = d%e;
+console.log (`Остаток от деления 9 и 2= ${divide}`)
 
 // Task 7
 let num = 1;
@@ -42,66 +42,75 @@ num *= 7;
 num /= 3;
 num = ++num;
 num = --num;
-alert(num);
+console.log (num);
 
 //Task 8
-let age=prompt (`Сколько вам лет?`)
-alert (age)
+let age = prompt(`Сколько вам лет?`)
+console.log (age)
 
 // Task 9.0
-const user= {name: `Pelmeshki`,age:10,isAdmin:true};
-alert (user.name);
-alert (user.age);
-alert (user.isAdmin);
+const user = {
+    name: `Pelmeshki`,
+    age:10,
+    isAdmin:true,
+};
+
+console.log (user.name);
+console.log (user.age);
+console.log (user.isAdmin);
 
 //Task 9.1
 user [`City of residence`];
-alert ([`City of residence`]);
+console.log ([`City of residence`]);
 
 // Task 9.2
-user.age=15;
-alert(user.age);
+user.age = 15;
+console.log (user.age);
 
 // Task 9.3
 delete user [`City of residence`];
 
 // Task 9.4
-let info=prompt (`Какую информацию хотите узнать о пользователе?`)
+let info = prompt (`Какую информацию хотите узнать о пользователе?`)
 alert (user[info])
 
 // Task 10
-let userName=prompt (`Как вас зовут?`);
+let userName = prompt(`Как вас зовут?`);
 alert (`Привет, ${userName} !`)
 
 // Hw3 
 //Task 1
-let password=`Пароль`;
-let enterPassword= prompt (`Введите пароль`);
-enterPassword=enterPassword.toLowerCase ();
-if (enterPassword===`password`) {alert (`Пароль введен верно`);
-} else {alert (`Пароль введен неправильно`);}
+let password =`Пароль`;
+let enterPassword = prompt(`Введите пароль`);
+enterPassword = enterPassword.toLowerCase ();
+
+if (enterPassword ===`password`) {
+    alert (`Пароль введен верно`);
+
+} else {
+    alert (`Пароль введен неправильно`);}
 
 // Task 2 
-c=3;
-if (c>=0 && c<=10) {alert (`Верно`);   
-} else {alert (`Неверно`);    
+c = 3;
+if (c >= 0 && c <= 10) {console.log (`Верно`);   
+} else {console.log (`Неверно`);    
 }
 
 // Task 3
 let f = 50;
 let g = 120;
-if (f>100 || g>100) {
-	alert('Верно');
-} else {alert (`Неверно`);
+if (f > 100 || g > 100) {
+	console.log ('Верно');
+} else {console.log (`Неверно`);
 }
 
 //Task 4
-a='2';
-b='3';
-alert (Number(a) + Number(b))
+a ='2';
+b ='3';
+console.log (Number(a) + Number(b))
 
 // Task 5
-let monthNumber=Number(prompt(`Введите номер месяца`));
+let monthNumber = Number(prompt(`Введите номер месяца`));
 switch (monthNumber) {
     case 1:
     case 2:
@@ -129,14 +138,14 @@ switch (monthNumber) {
 
 // Hw4 
 //Task 1
-let i=1;
-while (i<3) {alert (`Привет`); 
+let i = 1;
+while (i < 3) {alert (`Привет`); 
 i++;
 }
 
  //Task 2
- i=1;
- while (i<=5) {console.log (i)
+ i = 1;
+ while (i <= 5) {console.log (i)
     i++;   
  }
 
@@ -148,30 +157,31 @@ do {
 } while (i <= 22);
 
 // Task 4
-const object= {
+const object = {
 'Коля':'200',
 'Вася':'300',
 'Петя':'400'
 };
+
 for (const key in object) {
         const element = object[key];
         console.log (`${key} - зарплата ${element} долларов`);    
     }
 
 // Task 5
-let n=1000
-let minNum=0
-for (; n>=50; n/=2){
+let n = 1000
+let minNum = 0
+for (; n>= 50; n/= 2){
     minNum++;
 }
 console.log (n);
 console.log (minNum)
 
 // Task 6
-let dayNum=1;
+let dayNum = 1;
 do {console.log (`Сегодня пятница, ${dayNum}-е число. Необходимо подготовить отчет.`);
-dayNum+=7;   
-} while (dayNum<=31);
+dayNum+= 7;   
+} while (dayNum<= 31);
 
 //Hw5
 // Task 1
